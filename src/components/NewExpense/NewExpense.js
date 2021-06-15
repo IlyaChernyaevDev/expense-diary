bab2d1c2e810f9e55bb72cb95d9029cf241a6357
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-06-15 09:14:23 +0300

    added functionality for showing and hiding ExpenseForm

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [showExpenseForm, setShowExpenseForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setShowExpenseForm(false);
   };
+
+  let newExpenseContent = <button onClick={() => setShowExpenseForm(true)}>Add New Expense</button>;
+
+  if(showExpenseForm) {
+    newExpenseContent = <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} cancel={setShowExpenseForm}/>;
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {newExpenseContent}
     </div>
   );
 };
